c26556bd6562361b0b7b791572fa652a23b62958
Abhishek <abhishekkasni99@gmail.com>
2025-07-17 23:51:17 +0530

    First version of the app, in this version i have added basic uv, created a mini app 'tweet' inside the main project. Added forms.py, views.py, etc.

---
+++ chaiThreads/chaiThreads/tweet/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from .models import Tweet
+from .forms import TweetForm
+from django.shortcuts import get_list_or_404
+# Create your views here.
+def index(request):
+    return render(request,'index.html')
+
+def tweet_list(request):
+    Tweet.objects.all().order_by('-created_at')
+    return render(request,'tweet_list.html',{'tweets': tweets})
+
+def tweet_create(request):
+    if request.method=='POST':
+        form = TweetForm(request.POST, request.FILES)
+    else:
+        form = TweetForm()
+    return render(request,'tweet_form.html',{'form':form})
